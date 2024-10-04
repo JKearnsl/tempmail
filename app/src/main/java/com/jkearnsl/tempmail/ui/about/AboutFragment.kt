3e9eb61534959f66a4fa3251bc3164f075894609
JKearnsl <boyarshinovn@gmail.com>
2024-10-04 14:44:02 +0300

    update: added policy_text html format

--- app/src/main/java/com/jkearnsl/tempmail/ui/about/AboutFragment.kt
+++ app/src/main/java/com/jkearnsl/tempmail/ui/about/AboutFragment.kt
@@ -1,20 +1,20 @@
 package com.jkearnsl.tempmail.ui.about
 
 import android.os.Bundle
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.jkearnsl.tempmail.R
 import com.jkearnsl.tempmail.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
 
     private var _binding: FragmentAboutBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,11 +28,14 @@ class AboutFragment : Fragment() {
         _binding = FragmentAboutBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textAbout
-        aboutViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val policyTextView: TextView = view.findViewById(R.id.policy_text)
+        policyTextView.text = Html.fromHtml(getString(R.string.policy_text), Html.FROM_HTML_MODE_LEGACY)
     }
 
     override fun onDestroyView() {
