efa981b1f9e442149c28a741fb671f549af989b2
JKearnsl <boyarshinovn@gmail.com>
2024-10-05 13:05:03 +0300

    update: now users can change own email

--- app/src/main/java/com/jkearnsl/tempmail/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/jkearnsl/tempmail/ui/settings/SettingsFragment.kt
@@ -1,21 +1,23 @@
 package com.jkearnsl.tempmail.ui.settings
 
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.jkearnsl.tempmail.TempMail
 import com.jkearnsl.tempmail.databinding.FragmentSettingsBinding
+import kotlinx.coroutines.launch
 
 class SettingsFragment : Fragment() {
 
     private var _binding: FragmentSettingsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private var rotationAnimator: ObjectAnimator? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,11 +30,40 @@ class SettingsFragment : Fragment() {
         _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSettings
-        settingsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val app = requireActivity().application as TempMail
+        val core = app.core
+
+        startIconRotation()
+        lifecycleScope.launch {
+            binding.textInputEditText.setText(core.currentEmail())
+            stopIconRotation()
+        }
+
+
+        binding.textInputLayout.setEndIconOnClickListener {
+            startIconRotation()
+            lifecycleScope.launch {
+                binding.textInputEditText.setText(core.generateRandomEmail())
+                stopIconRotation()
+            }
         }
         return root
+    }
+
+    @SuppressLint("ObjectAnimatorBinding")
+    private fun startIconRotation() {
+        val endIconView = binding.textInputLayout.findViewById<View>(com.google.android.material.R.id.text_input_end_icon)
+        endIconView?.let {
+            rotationAnimator = ObjectAnimator.ofFloat(it, "rotation", 0f, -360f).apply {
+                duration = 1000 // 1 second
+                repeatCount = ObjectAnimator.INFINITE
+                start()
+            }
+        }
+    }
+
+    private fun stopIconRotation() {
+        rotationAnimator?.cancel()
     }
 
     override fun onDestroyView() {
