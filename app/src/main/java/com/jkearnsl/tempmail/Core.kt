03f98e9d9034cabad88ce88012925b660c40a2a1
JKearnsl <boyarshinovn@gmail.com>
2024-10-05 13:01:25 +0300

    added Core class

---
+++ app/src/main/java/com/jkearnsl/tempmail/Core.kt
@@ -0,0 +1,86 @@
+package com.jkearnsl.tempmail
+
+import android.content.SharedPreferences
+import com.jkearnsl.tempmail.ui.messages.Message
+import okio.IOException
+import org.json.JSONObject
+
+class Core() {
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private val api = Api()
+    public val messages = mutableListOf<Message>()
+
+    constructor(sharedPreferences: SharedPreferences): this() {
+        this.sharedPreferences = sharedPreferences
+        sharedPreferences.getStringSet("last_messages", emptySet())?.mapNotNull { messageString ->
+            val message = JSONObject(messageString)
+            Message(
+                message.getInt("id"),
+                message.getString("subject"),
+                message.getString("from"),
+                message.getString("date")
+            )
+        }?.let { messages.addAll(it) }
+    }
+
+    suspend fun currentEmail(): String {
+        val email = sharedPreferences.getString("current_email", "null")?: "null"
+        return if (email == "null") {
+            try {
+                api.generateRandomEmail()
+            } catch (e: IOException) {
+                "null@null.com"
+            }
+        } else {
+            email
+        }
+    }
+
+    suspend fun generateRandomEmail(): String {
+        val email = api.generateRandomEmail()
+        sharedPreferences.edit().putString("current_email", email).apply()
+        sharedPreferences.edit().putStringSet("last_messages", emptySet()).apply()
+        return email
+    }
+
+    suspend fun refreshMessages() {
+        val email = sharedPreferences.getString("current_email", "null")?: "null"
+        if (email == "null") {
+            return
+        }
+
+        try {
+            val newMessages = api.getMessages(email)
+            sharedPreferences.edit().putStringSet(
+                "last_messages",
+                newMessages.map { message ->
+                    JSONObject(message).toString()
+                }.toSet()
+            ).apply()
+            this.messages.clear()
+            this.messages.addAll(newMessages.map { message ->
+                Message(
+                    message["id"] as Int,
+                    message["subject"] as String,
+                    message["from"] as String,
+                    message["date"] as String
+                )
+            })
+        } catch (e: IOException) {
+            // Do nothing
+        }
+    }
+
+    fun fetchMessage(email: String, id: Int): Map<String, Any> {
+        return emptyMap()
+    }
+
+    fun fetchAttachment(email: String, id: Int, attachmentId: Int): ByteArray {
+        return ByteArray(0)
+    }
+
+    fun deleteAttachment(email: String, id: Int, attachmentId: Int): Boolean {
+        return false
+    }
+}
