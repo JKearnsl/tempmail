09fd1296b3f0d22da637dd01643c059f51d1af96
JKearnsl <boyarshinovn@gmail.com>
2024-10-06 11:39:47 +0300

    update: added content, attachments

--- app/src/main/java/com/jkearnsl/tempmail/ui/messages/Message.kt
+++ app/src/main/java/com/jkearnsl/tempmail/ui/messages/Message.kt
@@ -1,39 +1,11 @@
 package com.jkearnsl.tempmail.ui.messages
-import android.os.Parcel
-import android.os.Parcelable
 import java.util.Date
 
 data class Message(
     val id: Int,
     val subject: String,
     val email: String,
-    val date: Date
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString()!!,
-        parcel.readString()!!,
-        Date(parcel.readLong())
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(subject)
-        parcel.writeString(email)
-        parcel.writeLong(date.time)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Message> {
-        override fun createFromParcel(parcel: Parcel): Message {
-            return Message(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Message?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    val date: Date,
+    val content: String,
+    val attachments: List<Attachment>
+)
